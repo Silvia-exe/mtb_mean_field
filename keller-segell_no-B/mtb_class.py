f791044c081faddfd66b72ff3aa9935c6851db12
Silvia Andrea Morales Manzano <moralesmanza@Silik.theorie.physik.uni-goettingen.de>
2026-03-27 16:46:47 +0100

    New model, rho and theta. Added B. Last working version in folder.

--- keller-segell_no-B/mtb_class.py
+++ keller-segell_no-B/mtb_class.py
@@ -16,7 +16,6 @@ class mtb:
         
         params={
             'save_every': int(config['metadata']['save_every']),
-
             'Lx': float(config['grid']['Lx']),
             'Ly': float(config['grid']['Ly']),
             'Nx': int(config['grid']['Nx']),
@@ -26,9 +25,6 @@ class mtb:
 
             'D_o2': float(config['diff']['D_o2']),
             'D_bact': float(config['diff']['D_bact']),
-            'D_r': float(config['diff']['D_r']),
-            'gamma_r': float(config['diff']['gamma_r']),
-            'beta_p': float(config['diff']['beta_p']),
         
             'c0_o2': float(config['o2']['c0_o2']),
             'ca_o2': float(config['o2']['ca_o2']),
@@ -41,9 +37,10 @@ class mtb:
             'chi': float(config['bact']['chi']),
             'b0': float(config['bact']['b0']),
             'mu': float(config['bact']['mu']),
-            'v0': float(config['bact']['v0']),
+            'gamma_r': float(config['bact']['gamma_r']),
 
-            'B_phi': float(config['magnetism']['B_phi']),
+            'Bx': float(config['magnetism']['Bx']),
+            'By': float(config['magnetism']['By']),
             'B': float(config['magnetism']['B']),
         }
 
@@ -69,9 +66,6 @@ class mtb:
 
         self.D_o2 = params['D_o2']
         self.D_bact = params['D_bact']
-        self.D_r = params['D_r']
-        self.gamma_r = params['gamma_r']
-        self.beta_p = params['beta_p']
         self.c0_o2 = params['c0_o2']
         self.ca_o2 = params['ca_o2']
         self.copt_o2 = params['copt_o2']
@@ -82,10 +76,11 @@ class mtb:
         self.chi = params['chi']
         self.b0 = params['b0']
         self.mu = params['mu']
-        self.v0 = params['v0']
+        self.gamma_r = params['gamma_r']
 
         self.B = params['B']
-        self.B_phi = params['B_phi']
+        self.Bx = params['Bx']
+        self.By = params['By']
 
         self.dx, self.dy = self.Lx/(self.Nx-1), self.Ly/(self.Ny-1)
         self.t = np.arange(0,self.T,self.dt)
@@ -93,21 +88,23 @@ class mtb:
 
         self.b = CellVariable(mesh = self.mesh, name = 'bacteria', value = self.b0)
         self.c = CellVariable(mesh = self.mesh, name = 'oxygen', value = self.cmin_o2)
-        self.theta = CellVariable(mesh = self.mesh, name = 'polarization_y', value = 0.0)
-        self.theta.setValue(np.random.uniform(-np.pi, np.pi, size = self.mesh.numberOfCells))
+        self.p = CellVariable(mesh = self.mesh, name = 'polarization', value = 0.0)
         self.c.constrain(self.c0_o2, where = self.mesh.facesLeft)
 
-        self.Bx = self.B*np.cos(self.B_phi)
-        self.By = self.B*np.sin(self.B_phi)
+        self.Bx = self.B*self.Bx
+        self.By = self.B*self.By
+
+        self.m = CellVariable(mesh = self.mesh, name = 'velocity', rank = 1, value = (1.0,0.0))
+
+        
     
-    def init_h5(self, save_every = 20, filename = 'mtb_simulation_B0_T1000.h5'):
+    def init_h5(self, save_every = 20, filename = 'mtb_simulation.h5'):
         self.h5file = h5py.File(filename, 'w')
 
         Nt_save = int(len(self.t) // save_every + 1)
 
         self.dset_c = self.h5file.create_dataset('o2', shape = (Nt_save, self.Ny, self.Nx), dtype = 'f4', chunks = (1, self.Ny, self.Nx), compression = 'gzip')
         self.dset_b = self.h5file.create_dataset('bacteria', shape = (Nt_save, self.Ny, self.Nx), dtype = 'f4', chunks = (1, self.Ny, self.Nx), compression = 'gzip')
-        self.dset_theta = self.h5file.create_dataset('theta', shape =(Nt_save, self.Ny, self.Nx), dtype= 'f4', chunks = (1,self.Ny, self.Nx), compression = 'gzip')
         self.dset_t = self.h5file.create_dataset('t', shape = (Nt_save,), dtype = 'f4')
 
         self.h5file.attrs['Lx'] = self.Lx
@@ -120,6 +117,7 @@ class mtb:
         self.h5file.attrs['D_bact'] = self.D_bact
         self.h5file.attrs['c0_o2'] = self.c0_o2
         self.h5file.attrs['ca_o2'] = self.ca_o2
+        self.h5file.attrs['copt_o2'] = self.copt_o2
         self.h5file.attrs['cmin_o2'] = self.cmin_o2
         self.h5file.attrs['copt_o2'] = self.copt_o2
         self.h5file.attrs['p'] = self.p
@@ -127,11 +125,6 @@ class mtb:
         self.h5file.attrs['K'] = self.K
         self.h5file.attrs['chi'] = self.chi
         self.h5file.attrs['b0'] = self.b0
-        self.h5file.attrs['mu'] = self.mu
-        self.h5file.attrs['gamma_r'] = self.gamma_r
-
-        self.h5file.attrs['B'] = self.B
-        self.h5file.attrs['B_phi'] = self.B_phi
 
         return self.h5file
 
@@ -139,8 +132,16 @@ class mtb:
 
         self.dset_c[i] = self.c.value.reshape ((self.Ny, self.Nx))
         self.dset_b[i] = self.b.value.reshape((self.Ny, self.Nx))
-        self.dset_theta[i] = self.theta.value.reshape((self.Ny, self.Nx))
         self.dset_t[i] = t
+
+    def aerotaxis_vectors(self):
+        o2_diff = (self.c - self.copt_o2) / (self.c + self.copt_o2 + self.eps)
+        v = -self.m * self.chi * o2_diff * self.c.grad
+
+        flux_x = self.b.value * v[0].value
+        flux_y = self.b.value * v[1].value
+
+        return [flux_x, flux_y]
     
     def consumption_magnitude(self):
         consumption = (self.consumption()*self.b).value
@@ -148,33 +149,14 @@ class mtb:
         return consumption
     
     def growth_magnitude(self):
-        growth = (self.p * self.b * (1-(self.b/self.K))).value
+        growth = self.p * self.b * (1-(self.b/self.K))
         growth = growth.reshape((self.Ny, self.Nx))
         return growth
-
-    def velocity_vectors(self):
-        # Active swimming velocity from orientation
-        Px_f = np.cos(self.theta.value)
-        Py_f = np.sin(self.theta.value)
-        v_swim_x = self.v0 * Px_f
-        v_swim_y = self.v0 * Py_f
-        
-        # Aerotactic velocity component
-        o2_grad = self.c.grad
-        o2_diff = (self.c - self.copt_o2) / (self.c + self.copt_o2 + self.eps)
-        v_aero_x = -self.chi * o2_diff * o2_grad[0]
-        v_aero_y = -self.chi * o2_diff * o2_grad[1]
-        
-        # Total velocity = active swimming + aerotaxis
-        v_total_x = (v_swim_x + v_aero_x.value).reshape(v_swim_x.shape)
-        v_total_y = (v_swim_y + v_aero_y.value).reshape(v_swim_y.shape)
-        
-        return (v_total_x, v_total_y)
 
     def init_oxygen(self, o2_file = None):
         if o2_file is None:
             eq = TransientTerm(var = self.c) == DiffusionTerm(coeff = self.D_o2, var = self.c)
-            for _ in tqdm(range(500), desc="Initializing O2 gradient from scratch..."):
+            for _ in tqdm(range(3000), desc="Initializing O2 gradient from scratch..."):
                 eq.solve(dt = 4e-1)
             filename = 'mtb_oxygen_init_Nx'+str(self.Nx)+'_Ny'+str(self.Ny)+'.h5'
             with h5py.File(filename, 'w') as f:
@@ -214,72 +196,22 @@ class mtb:
 
     def build_equations(self):
 
-        #CELL QUANTITIES
-     
-        o2_grad = self.c.grad
-
-        cx = o2_grad[0]
-        cy = o2_grad[1]
-
-        #o2_diff = (self.c - self.copt_o2) / (self.c + self.copt_o2 + self.eps)
-
-        #AEROTAXIS (bacteria seek c_opt)
-
-        c_face = self.c.arithmeticFaceValue
-        b_face = self.b.arithmeticFaceValue
-        grad_c_face = self.c.faceGrad
-
-        # Preference signal: negative when c < c_opt (prefer to increase c), positive when c > c_opt
-        o2_diff_face = (c_face - self.copt_o2) / (c_face + self.copt_o2 + self.eps)
-
-        # Aerotactic flux: chi * (c - c_opt) * grad_c / (c + c_opt)
-        flux_x = -self.chi * o2_diff_face * grad_c_face[0]
-        flux_y = -self.chi * o2_diff_face * grad_c_face[1]
-
-        aerotaxis_flux = FaceVariable(mesh=self.mesh, rank=1, value=(flux_x, flux_y))
-
-        #MAGNETIC ALIGNMENT
-        theta_B = np.arctan2(self.By,self.Bx)
-        mag_torque = self.gamma_r*np.sin(theta_B - self.theta)
-
-        #CHEMOTACTIC TORQUE
-    
-        c_cross_P = cx.value * np.sin(self.theta) - cy.value * np.cos(self.theta)
-        chem_torque = self.beta_p * c_cross_P
-
-        # ACTIVE VELOCITY
-        Px = np.cos(self.theta.arithmeticFaceValue)
-        Py = np.sin(self.theta.arithmeticFaceValue)
-
-        v = FaceVariable(mesh=self.mesh, rank=1, value=(self.v0 * Px, self.v0 * Py))
-
-        #EQUATIONS
-
-        # ---- Density ----
-        # Combines: diffusion + active swimming + aerotaxis + logistic growth
-        self.eq_b = ( TransientTerm(var=self.b) == DiffusionTerm(coeff=self.D_bact, var=self.b) - ConvectionTerm(coeff=v, var=self.b) - ConvectionTerm(coeff=aerotaxis_flux, var=self.b) + self.p * self.b * (1 - self.b / self.K))
-
-        # ---- Oxygen ----
-        self.eq_c = ( TransientTerm(var=self.c)== DiffusionTerm(coeff=self.D_o2, var=self.c)- self.consumption() * self.b)
-
-        # ---- Polarization ----
-        self.eq_theta = (TransientTerm(var=self.theta) == DiffusionTerm(coeff=self.D_r, var=self.theta) + chem_torque + mag_torque)
-        
+        self.eq_c = TransientTerm(var=self.c) == DiffusionTerm(coeff = self.D_o2, var = self.c) - self.consumption()*self.b
+        o2_diff = (self.c - self.copt_o2)/(self.c+self.copt_o2+self.eps)
+        v0 = - self.chi * o2_diff * self.c.grad
+        self.eq_b = TransientTerm( var = self.b) == DiffusionTerm(coeff = self.D_bact, var = self.b) \
+                                                - ConvectionTerm(coeff = v0, var = self.b) \
+                                                + self.p * self.b * (1-(self.b/self.K))
     def run(self):
 
-        self.c = self.init_oxygen(self.O2_init) 
+        #self.c = self.init_oxygen(self.O2_init) 
         #self.b = self.inoculum_center()
         self.build_equations()
         
         for _ in tqdm(self.t, desc="Running..."):
-            self.c.value = np.clip(self.c.value,self.cmin_o2,self.c0_o2)
-            #self.b.value = np.clip(self.b.value, 0.0, None)
-            #self.theta.value = ((self.theta.value + np.pi) %(2*np.pi))-np.pi
-
             self.eq_c.solve(dt = self.dt)
             self.eq_b.solve(dt = self.dt)
-            self.eq_theta.solve(dt = self.dt)
-            
+
     def run_save(self):
 
         self.h5file = self.init_h5()
@@ -291,12 +223,6 @@ class mtb:
         for i, t in tqdm(enumerate(self.t), total = len(self.t), desc = 'Running and saving...'):
             self.eq_c.solve(dt = self.dt)
             self.eq_b.solve(dt = self.dt)
-            self.eq_theta.solve(dt = self.dt)
-           
-            self.c.value = np.clip(self.c.value,self.cmin_o2,self.c0_o2)
-            #self.b.value = np.clip(self.b.value, 0.0, None)
-            #self.theta.value = ((self.theta.value + np.pi) %(2*np.pi))-np.pi
-            
             if i% self.save_every == 0:
                 self.save_state_h5(save_idx, t)
                 save_idx += 1
